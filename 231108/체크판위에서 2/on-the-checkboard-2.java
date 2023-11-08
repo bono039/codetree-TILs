9c7b927fdadf0747e2b869ea1c983db91dfae207
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-11-08 13:17:55 +0900

    Title: 체크판위에서 2, Time: 108ms, Memory: 8MB, Status: Passed - CodeTree

--- 231108/체크판위에서 2/on-the-checkboard-2.java
+++ 231108/체크판위에서 2/on-the-checkboard-2.java
@@ -22,8 +22,8 @@ public class Main {
         }
 
         int cnt = 0;
-        for(int i = 0 ; i < R ; i++) {
-            for(int j = 0 ; j < C ; j++) {
+        for(int i = 1 ; i < R ; i++) {
+            for(int j = 1 ; j < C ; j++) {
                 for(int k = i + 1 ; k < R - 1 ; k++) {
                     for(int l = j + 1 ; l < C - 1 ; l++) {
                         if(map[0][0] != map[i][j] && map[i][j] != map[k][l] && map[k][l] != map[R-1][C-1])
