c59f52443dcf96b8d5e70d1b7afd1d327ea2427f
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-11-08 13:14:07 +0900

    Title: 체크판위에서 2, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231108/체크판위에서 2/on-the-checkboard-2.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int R, C;
+    static char[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        // 입력받기
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        map = new char[R][C];
+        for(int i = 0 ; i < R ; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0 ; j < C ; j++) {
+                map[i][j] = st.nextToken().charAt(0);
+            }
+        }
+
+        int cnt = 0;
+        for(int i = 0 ; i < R ; i++) {
+            for(int j = 0 ; j < C ; j++) {
+                for(int k = i + 1 ; k < R - 1 ; k++) {
+                    for(int l = j + 1 ; l < C - 1 ; l++) {
+                        if(map[0][0] != map[i][j] && map[i][j] != map[k][l] && map[k][l] != map[R-1][C-1])
+                            cnt++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
