adad75c5ac12c15ad9c7bbf67895b1b8c5b89c94
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-11-08 16:11:58 +0900

    Title: 원 모양으로 되어있는 방, Time: 108ms, Memory: 9MB, Status: Passed - CodeTree

--- 231108/원 모양으로 되어있는 방/a-room-in-a-circle.java
+++ 231108/원 모양으로 되어있는 방/a-room-in-a-circle.java
@@ -5,7 +5,9 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
+        
         int[] arr = new int[N];
         for(int i = 0 ; i < N ; i++) {
             arr[i] = Integer.parseInt(br.readLine());
@@ -14,11 +16,10 @@ public class Main {
         // 시작점 돌아가면서 설정해보고, 그 중 최솟값 구해보기
         int min = Integer.MAX_VALUE;
         for(int i = 0 ; i < N ; i++) {
-            int num = i;
             int tmp = 0;
 
             for(int j = 0 ; j < N ; j++) {
-                if(j == num)    continue;
+                if(j == i)    continue;
 
                 if(j >= i) {
                     tmp += (j - i) * arr[j];
