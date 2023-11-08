c5cc1c5eb320ef38187d4c1cdcd3e000ca4b0790
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-11-08 12:05:00 +0900

    Title: 씨 오 더블유 2, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

--- 231108/씨 오 더블유 2/c-o-w-2.java
+++ 231108/씨 오 더블유 2/c-o-w-2.java
@@ -7,18 +7,17 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         char[] ch = br.readLine().toCharArray();
-        int[] arr = new int[3];
 
-        for(int i = 0 ; i < ch.length ; i++) {
-            if(ch[i] == 'C')        arr[0]++;
-            else if(ch[i] == 'O')   arr[1]++;
-            else if(ch[i] == 'W')   arr[2]++;
+        int cnt = 0;
+        for(int i = 0 ; i < N - 2 ; i++) {
+            for(int j = i + 1 ; j < N - 1 ; j++) {
+                for(int k = j + 1 ; k < N ; k++) {
+                    if(ch[i] == 'C' && ch[j] == 'O' && ch[k] == 'W') {
+                        cnt++;
+                    }
+                }
+            }
         }
-
-        int answer = 1;
-        for(int i : arr) {
-            answer *= i;
-        }
-        System.out.println(answer);
+        System.out.println(cnt);
     }
 }
