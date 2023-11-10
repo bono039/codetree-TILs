30c2593e0edb22cd9a29900baf7ed948f4e1a932
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-11-10 17:01:32 +0900

    Title: G or H 2, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

--- 231110/G or H 2/G-or-H-2.java
+++ 231110/G or H 2/G-or-H-2.java
@@ -2,59 +2,41 @@ import java.util.*;
 import java.io.*;
 // 양쪽 끝에 있는 사람 간의 거리
 
-class Point {
-    int idx; char c;
-
-    public Point(int idx, char c) {
-        this.idx = idx;
-        this.c = c;
-    }
-}
-
 public class Main {
     static final int MAX_NUM = 100;
 
     public static void main(String[] args) throws IOException {
-        // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        Point[] arr = new Point[MAX_NUM + 1];
+        int[] arr = new int[MAX_NUM + 1];
 
         for(int i = 0 ; i < N ; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int G = Integer.parseInt(st.nextToken());
             char c = st.nextToken().charAt(0);
 
-            arr[i] = new Point(G, c);
-        }
-
-        if(N == 1) {
-            System.out.println(0);
-            return;
+            arr[G] = (c == 'G') ? 1 : 2;
         }
 
         int answer = 0;
-        for(int i = 0 ; i < N ; i++) {  // 시작점
-            int[] tmp = new int[2];
-            int start = arr[i].idx;
-            int end = 0;
+        for(int i = 1 ; i <= MAX_NUM ; i++) {
+            if(arr[i] == 0)  continue;
+            int g = 0;
+            int h = 0;
 
-            for(int j = i ; j < N ; j++) {  // 끝점
-                end = arr[j].idx;
-                //System.out.print(end + " > ");
+            for(int j = i ; j <= MAX_NUM ; j++) {
+                if(arr[j] == 0) continue;
+                else if(arr[j] == 1) g++;
+                else    h++;
 
-                if(arr[j].c == 'G') tmp[0]++;
-                else if(arr[j].c == 'H') tmp[1]++;
-
-                if(tmp[0] == tmp[1]) {
-                    if(tmp[0] != 0)
-                        answer = Math.max(answer, end - start);
+                if(g == 0 || h == 0 || g == h) {
+                    answer = Math.max(answer, j - i);
                 }
             }
         }
 
-        System.out.println(answer + 1);
+        System.out.println(answer);
     }
 }
