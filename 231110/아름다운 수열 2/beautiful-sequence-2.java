51a66ecc07f711863410b16ae5e8d41586a4f5b8
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-11-10 12:46:39 +0900

    Title: 아름다운 수열 2, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

--- 231110/아름다운 수열 2/beautiful-sequence-2.java
+++ 231110/아름다운 수열 2/beautiful-sequence-2.java
@@ -6,6 +6,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
+        // 입력받기
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
@@ -20,22 +21,28 @@ public class Main {
         for(int i = 0 ; i < M ; i++) {
             B[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(B); // 정렬하기
 
         int answer = 0;
-        for(int i = 0 ; i <= N - M ; i++) {
-            int tmp = 0;
-
-            for(int j = i ; j < i + M ; j++) {
-                for(int k = 0 ; k < M ; k++) {
-                    if(A[j] == B[k]) {
-                        tmp++;
-                    }
-                }
-                if(tmp == M) answer++;
+        for(int i = 0 ; i <= N - M ; i++) { // 탐색할 A 배열 구간에서의 시작점
+            int[] tmp = new int[M]; // 구간 내 값들을 담을 임시 배열
+            int idx = 0;
+            for(int j = i ; j < i + M ; j++) {  // 탐색할 A 배열 구간에서의 끝점
+                tmp[idx++] = A[j];
             }
+
+            // 정렬 후 두 배열의 값이 같다면, 아름다운 수열 수 + 1
+            Arrays.sort(tmp);
+            boolean isSame = true;
+            for(int j = 0 ; j < M ; j++) {
+                if(tmp[j] != B[j]) {
+                    isSame = false;
+                }
+            }
+
+            if(isSame) answer++;
         }
         System.out.println(answer);
 
-        // 백트래킹 아니묘?
     }
 }
