dee99d1a6fb6de4d83189bd27ec111f40c2293f0
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-11-15 01:26:56 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 96ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 231115/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 231115/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -2,28 +2,55 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static final int OFFSET = 1000;
+    static int[][] grid = new int[2001][2001];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         // 첫 번쨰 직사각형
-        int x1 = Integer.parseInt(st.nextToken());
-        int y1 = Integer.parseInt(st.nextToken());
-        int x2 = Integer.parseInt(st.nextToken());
-        int y2 = Integer.parseInt(st.nextToken());
+        int x1 = Integer.parseInt(st.nextToken()) + OFFSET;
+        int y1 = Integer.parseInt(st.nextToken()) + OFFSET;
+        int x2 = Integer.parseInt(st.nextToken()) + OFFSET;
+        int y2 = Integer.parseInt(st.nextToken()) + OFFSET;
+        paint(x1, y1, x2, y2, 1);
 
         // 두 번째 직사각형
         st = new StringTokenizer(br.readLine(), " ");
-        int x3 = Integer.parseInt(st.nextToken());
-        int y3 = Integer.parseInt(st.nextToken());
-        int x4 = Integer.parseInt(st.nextToken());
-        int y5 = Integer.parseInt(st.nextToken());
+        int x3 = Integer.parseInt(st.nextToken()) + OFFSET;
+        int y3 = Integer.parseInt(st.nextToken()) + OFFSET;
+        int x4 = Integer.parseInt(st.nextToken()) + OFFSET;
+        int y4 = Integer.parseInt(st.nextToken()) + OFFSET;
+        paint(x3, y3, x4, y4, 2);
 
-        int total = 0;
+        int minX = 2000;    int minY = 2000;
+        int maxX = 0;       int maxY = 0;
 
-        int diffX = Math.abs(x2 - x1);
-        int diffY = Math.abs(y2 - y1);
-        total = diffX * diffY;
+        for(int i = x1 ; i <= x2 ; i++) {
+            for(int j = y1 ; j <= y2 ; j++) {
+                if(grid[i][j] == 1) {
+                    //System.out.print("HI");
+                    minX = Math.min(minX, i);
+                    maxX = Math.max(maxX, i);
 
-        System.out.println(total);
+                    minY = Math.min(minY, j);
+                    maxY = Math.max(maxY, j);
+                }
+            }
+        }
+        
+        if(minX == 2000 && minY == 2000 && maxX == 0 && maxY == 0)
+            System.out.println(0);
+        //System.out.println(minX + " " + minY + " " + maxX + " " + maxY);
+        else
+            System.out.println(Math.abs(maxX - minX) * Math.abs(maxY - minY));
+    }
+
+    private static void paint(int x1, int y1, int x2, int y2, int num) {
+        for(int i = x1 ; i <= x2 ; i++) {
+            for(int j = y1 ; j <= y2 ; j++) {
+                grid[i][j] = num;
+            }
+        }
     }
 }
