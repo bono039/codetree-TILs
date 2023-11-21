90cb45bfd2e4bfa590b767179b2f6180c075012a
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-11-21 20:38:08 +0900

    Title: 특정 수와 근접한 합, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231121/특정 수와 근접한 합/sum-close-to-particular-number.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        int total = 0;
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < N ; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            total += arr[i];
+        }
+
+        int diff = Integer.MAX_VALUE;
+        for(int i = 0 ; i < N ; i++) {
+            for(int j = 0 ; j < N ; j++) {  // 제외시킬 번호1
+                for(int k = 0 ; k < N ; k++) {  // 제외시킬 번호2
+                    if(Math.abs(S - (total - arr[j] - arr[k])) < diff) {
+                        diff = Math.abs(S - (total - arr[j] - arr[k]));
+                    }
+                }
+            }
+        }
+
+        System.out.println(diff);
+    }
+}
