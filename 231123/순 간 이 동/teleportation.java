404358651993dfa807e63b6e24a22c80b511f23e
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-11-23 22:09:57 +0900

    Title: 순 간 이 동, Time: 82ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231123/순 간 이 동/teleportation.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        int answer = 0;
+        int pos = A;
+        if(((int)Math.abs(A - x) < (int)Math.abs(A - y))) {
+            answer += (int)Math.abs(A - x);
+            answer += (int)Math.abs(B - y);
+        }
+        else {
+            answer += (int)Math.abs(A - y);
+            answer += (int)Math.abs(B - x);
+        }
+        System.out.println(answer);
+    }
+}
