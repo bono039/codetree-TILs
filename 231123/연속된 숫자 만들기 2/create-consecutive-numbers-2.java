f3d406608a79b5e652444c1872002b42536b3970
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-11-23 22:55:56 +0900

    Title: 연속된 숫자 만들기 2, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

--- 231123/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
+++ 231123/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
@@ -11,12 +11,16 @@ public class Main {
         int c = Integer.parseInt(st.nextToken());
 
         int answer = 0;
+
+        // Case1) 숫자 3개가 연속한 경우
         if((a + 1 == b) && (b + 1 == c)) {
             answer = 0;
         }
+        // Case2) 숫자 2개의 차이가 2인 경우
         else if(a + 2 == b || b + 2 == c) {
             answer = 1;
         }
+        // Case3) 그렇지 않은 경우, 항상 2번에 걸쳐 연속하게 만들 수 있음
         else {
             answer = 2;
         }
