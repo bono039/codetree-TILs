ecf462ce502197465569631db7026fe7dabdc9f9
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-11-24 17:27:50 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 88ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 231124/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
+++ 231124/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -1,13 +1,20 @@
 import java.util.*;
 import java.io.*;
 
-class Person {
+class Person implements Comparable<Person> {
     int t, x, y;
 
     public Person(int t, int x, int y) {
         this.t = t;
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public int compareTo(Person p) {
+        if (this.t != p.t)
+            return this.t - p.t;
+        return this.x - p.x;
     }
 }
 
@@ -21,9 +28,9 @@ public class Main {
         int P = Integer.parseInt(st.nextToken());
         int T = Integer.parseInt(st.nextToken());
 
-        Person[] people = new Person[N + 1];
-
-        for(int i = 0 ; i < T ; i++) {
+        Person[] people = new Person[T];
+        
+        for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int t = Integer.parseInt(st.nextToken());
             int x = Integer.parseInt(st.nextToken());
@@ -32,26 +39,21 @@ public class Main {
             people[i] = new Person(t, x, y);
         }
 
-        Arrays.sort(people, 0, T, new Comparator<Person>() {
-            @Override
-            public int compare(Person p1, Person p2) {
-                return p1.t - p2.t;
-            }
-        });
+        Arrays.sort(people);
 
-        int[] answer = new int[N + 1];  // 전염병 여부 배열
-        for(int i = 0 ; i < T ; i++) {
-            if(people[i].x == P || people[i].y == P) {
-                if(K > 0) {
+        int[] answer = new int[N + 1]; // 전염병 여부 배열
+        for (int i = 0; i < T; i++) {
+            if (people[i].x == P || people[i].y == P) {
+                if (K > 0) {
                     answer[people[i].x] = 1;
                     answer[people[i].y] = 1;
                     K--;
                 }
             }
         }
-     
+
         // 출력하기
-        for(int i = 1 ; i <= N ; i++) {
+        for (int i = 1; i <= N; i++) {
             System.out.print(answer[i]);
         }
     }
