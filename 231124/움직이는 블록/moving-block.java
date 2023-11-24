15d50247f1a484cbcf2b8704413a634244904a39
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-11-24 13:32:34 +0900

    Title: 움직이는 블록, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231124/움직이는 블록/moving-block.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        int cnt = 0;    // 움직여야 할 최소 블럭 수
+        int total = 0;
+        for(int i = 0 ; i < N ; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            total += arr[i];
+        }
+
+        int target = total / N;
+
+        for(int i = 0 ; i < N ; i++) {
+            if(arr[i] == target) continue;
+
+            if(arr[i] < target) {
+                for(int j = i + 1 ; j < N ; j++) {
+                    if(arr[j] > target) {
+                        int diff = target - arr[i];
+                        arr[i] = target;
+                        arr[j] -= diff;
+                        cnt += diff;
+                    }
+                }
+            }
+            else if(arr[i] > target) {
+                for(int j = i + 1 ; j < N ; j++) {
+                    if(arr[j] < target) {
+                        arr[j] += (arr[i] - target);
+                        cnt += (arr[i] - target);
+                        arr[i] -= target;
+                        break;
+                    }
+                }
+            }
+        }
+/
+        System.out.println(cnt);
+
+    }
+}
