7babd61993ab0801e742314961668eb28c6c14ca
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-11-25 22:01:10 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231125/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int K, N;
+    static int[] arr, result;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();                        
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        arr = new int[K];
+        for(int i = 0 ; i < arr.length ; i++) {
+            arr[i] = i + 1;
+        }
+        result = new int[N];
+        
+        back(0);
+        System.out.println(sb.toString());
+    }
+
+    private static void back(int depth) {
+        if(depth == N) {
+            for(int i : result) sb.append(i + " ");
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0 ; i < N ; i++) {
+            result[depth] = arr[i];
+            back(depth + 1);
+        }
+    }
+
+}
