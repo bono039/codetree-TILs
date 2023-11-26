15629f2a184731b19421f36752b97234ff3b974f
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-11-26 21:16:25 +0900

    Title: 사각형 채우기 3, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

--- 231126/사각형 채우기 3/rectangle-fill-3.java
+++ 231126/사각형 채우기 3/rectangle-fill-3.java
@@ -8,11 +8,16 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[1001];
+        long[] dp = new long[1001];
+        dp[0] = 1;
         dp[1] = 2;
-        dp[2] = 7;
-        for(int i = 3 ; i <= N ; i++) {
-            dp[i] = (dp[i - 1] * 3 + 1) % mod;
+
+        for(int i = 2 ; i <= N ; i++) {
+            dp[i] = (dp[i -1] * 2 + dp[i - 2] * 3) % mod;
+
+            for(int j = i - 3 ; j >= 0 ; j--) {
+                dp[i] = (dp[i] + dp[j] * 2) % mod;
+            }
         }
 
         System.out.println(dp[N]);
