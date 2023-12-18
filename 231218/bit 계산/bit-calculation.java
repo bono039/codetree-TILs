b67a74e66851f89efd46f5afe7c3e67f9d4c894b
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-12-18 13:29:52 +0900

    Title: bit 계산, Time: 510ms, Memory: 22MB, Status: Passed - CodeTree

--- 231218/bit 계산/bit-calculation.java
+++ 231218/bit 계산/bit-calculation.java
@@ -7,7 +7,7 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         int q = Integer.parseInt(br.readLine());
-        int A = 0;  // 비트마스크로 표현되는 집합
+        int S = 0;  // 비트마스크로 표현되는 집합
 
         while(q --> 0) {
             String[] str = br.readLine().split(" ");
@@ -16,32 +16,32 @@ public class Main {
                 int x = Integer.parseInt(str[1]);
 
                 // x 번째 비트가 0인지 확인하고, 0이면 해당 비트를 1로 변경
-                if((A & (1 << x)) != (1 << x))
-                    A ^= (1 << x);
+                if(((S >> x) & 1) == 0)
+                    S ^= (1 << x);
             }
             else if(str[0].equals("delete")) {
                 int x = Integer.parseInt(str[1]);
 
                 // x 번째 비트가 1인지 확인하고, 1이면 해당 비트를 0으로 변경
-                if((A & (1 << x)) == (1 << x)) {
-                    A ^= (1 << x);
+                if(((S >> x) & 1) == 1) {
+                    S ^= (1 << x);
                 }
             }
             else if(str[0].equals("print")) {
                 int x = Integer.parseInt(str[1]);
 
                 // x 번째 비트가 1인지 확인하여 결과를 StringBuilder에 추가
-                sb.append((A & (1 << x)) == (1 << x) ? 1 : 0).append("\n");
+                sb.append((S >> x) & 1).append("\n");
             }
             else if(str[0].equals("toggle")) {
                 int x = Integer.parseInt(str[1]);
                 
                 // x번째 비트 토글 (1이면 0으로, 0이면 1로)
-                A ^= (1 << x);
+                S ^= (1 << x);
             }
             else if(str[0].equals("clear")) {
                 // 집합 비움 = 공집합 (모든 비트를 0으로 변경)
-                A = 0;
+                S = 0;
             }
         }
 
