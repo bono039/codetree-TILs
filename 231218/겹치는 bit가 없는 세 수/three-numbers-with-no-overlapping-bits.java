88599ec8f5ed605fdb278b6e7b021c3abce65802
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-12-18 14:17:19 +0900

    Title: 겹치는 bit가 없는 세 수, Time: 1024ms, Memory: 80MB, Status: Passed - CodeTree

---
+++ 231218/겹치는 bit가 없는 세 수/three-numbers-with-no-overlapping-bits.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int max = 0;
+
+    static int N;
+    static int[] arr;
+    static List<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < N ; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 숫자 3개 고르기 (완탐 / 조합)
+        comb(0, 0);
+        System.out.println(max);
+
+        // bit가 서로 겹치지 않음 = & 연산하고 결과가 0일 때
+    }
+
+    private static void comb(int idx, int depth) {
+        if(depth == 3) {
+            if(chkBitOverlap()) {
+                max = Math.max(max, getSum());
+            }
+            return;
+        }
+
+        for(int i = idx ; i < arr.length ; i++) {
+            list.add(arr[i]);
+            comb(i + 1, depth + 1);
+            list.remove(list.size() - 1);
+        }
+
+        return;
+    }
+
+    private static boolean chkBitOverlap() {
+        int a = list.get(0);
+        int b = list.get(1);
+        int c = list.get(2);
+
+        return ((a & b) == 0 && (a & c) == 0 && (b & c) == 0);
+    }
+
+    private static int getSum() {
+        int sum = 0;
+        for(int i : list) {
+            sum += i;
+        }
+        return sum;
+    }
+}
