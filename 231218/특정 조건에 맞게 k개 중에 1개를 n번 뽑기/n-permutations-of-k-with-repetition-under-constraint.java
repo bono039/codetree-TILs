dcd77a8dc06a7c845022ce047ba3b2386acd4613
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-12-18 16:47:45 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 467ms, Memory: 35MB, Status: Passed - CodeTree

--- 231218/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
+++ 231218/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
@@ -3,22 +3,15 @@ import java.io.*;
 
 public class Main {
     static int K, N;
-    static int[] arr;
     static List<Integer> list = new ArrayList<>();
     static Set<String> set = new TreeSet<>();   // 결과 출력
-
-    static int lastNum = 0;
-    static int cnt = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        // K 이하 숫자 N번 고르기
         K = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-
-        arr = new int[K + 1];
 
         choose(0);
 
@@ -32,13 +25,12 @@ public class Main {
 
     private static void choose(int cnt) {
         if(cnt == N) {
-
             addToSet();
             return;
         }
 
-        // 단, 연속해서 숫자 3번 나오면 안 됨
         for(int i = 1 ; i <= K ; i++) {
+            // 같은 숫자가 연속해서 3번 나오는 경우, pass
             if(cnt >= 2 && i == list.get(list.size() - 1) && i == list.get(list.size() - 2))    continue;
             else {
                 list.add(i);
