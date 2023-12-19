4a38c0193b5915ca3608f249e17974a2bed578f0
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-12-20 00:36:10 +0900

    Title: xor 결과 최대 만들기, Time: 3051ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

--- 231220/xor 결과 최대 만들기/max-of-xor.java
+++ 231220/xor 결과 최대 만들기/max-of-xor.java
@@ -39,10 +39,10 @@ public class Main {
     }
 
     private static void xor() {
-        int num = 0;
+        int num = list.get(0);
 
-        for(int i = 0 ; i < list.size() - 1 ; i++) {
-            num ^= list.get(i + 1);
+        for(int i = 1 ; i < list.size() ; i++) {
+            num ^= list.get(i);
         }
 
         max = Math.max(max, num);
