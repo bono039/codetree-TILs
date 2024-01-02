dfffc2e569346e7b3adc6f244e8f36f6aba7eb2c
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-01-02 10:33:37 +0900

    Title: 테트리스 블럭 안의 합 최대화 하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240102/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.java
@@ -0,0 +1,83 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] dx = {-1,1,0,0};   // 상하좌우
+    static int[] dy = {0,0,-1,1};
+
+    static int N, M;
+    static int[][] map;
+    static boolean[][] visited;
+    static int max = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+
+        for(int i = 0 ; i < N ; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0 ; j < M ; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 테트리스 블럭 안의 최대 합 구하기
+        visited = new boolean[N][M];
+
+        for(int i = 0 ; i < N ; i++) {
+            for(int j = 0 ; j < N ; j++) {
+                // 4개 뽑는 dfs
+                visited[i][j] = true;
+                dfs(i, j, 1, map[i][j]);
+                visited[i][j] = false;
+
+                comb(0, 0, i, j, map[i][j]);    // 인접한 4칸 중 3칸 고르기 (ㅗ,ㅓ,ㅏ,ㅜ 모양)
+            }
+        }
+
+        System.out.println(max);
+    }
+
+    private static void dfs(int r, int c, int depth, int sum) {
+        if(depth == 4) {
+            max = Math.max(max, sum);
+            return;
+        }
+
+        for(int d = 0 ; d < 4 ; d++) {
+            int nr = r + dx[d];
+            int nc = c + dy[d];
+
+            if(!inRange(nr, nc) || visited[nr][nc])    continue;
+
+            visited[nr][nc] = true;
+            dfs(nr, nc, depth + 1, sum + map[nr][nc]);
+            visited[nr][nc] = false;
+        }
+    }
+
+    private static void comb(int start, int depth, int r, int c, int sum) {
+        if(depth == 3) {
+            max = Math.max(max, sum);
+            return;
+        }
+
+        for(int d = start ; d < 4 ; d++) {
+            int nr = r + dx[d];
+            int nc = c + dy[d];
+
+            if(!inRange(nr, nc))    continue;
+
+            comb(d + 1, depth + 1, r, c, sum + map[nr][nc]);
+        }
+    }
+
+    private static boolean inRange(int r, int c) {
+        return (0 <= r && r < N && 0 <= c && c < M);
+    }
+}
