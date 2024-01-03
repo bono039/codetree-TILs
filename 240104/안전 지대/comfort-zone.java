5d9fe93d1647fcc3b14fd8ba1205beba1082534e
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-01-04 00:11:18 +0900

    Title: 안전 지대, Time: 97ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240104/안전 지대/comfort-zone.java
+++ 240104/안전 지대/comfort-zone.java
@@ -48,7 +48,7 @@ public class Main {
             }
         }
 
-        System.out.println(ans + " " + K);
+        System.out.println(K + " " + ans);
     }
 
     private static void dfs(int x, int y, int v) {
