548b9bd00b248bdbcb26ca4060258d5171d17cb1
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-01-16 15:35:15 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1635ms, Memory: 88MB, Status: Passed - CodeTree

--- 240116/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 240116/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -2,58 +2,72 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-
-    static int N;
+    static int[] dx = {-1, 1,  0, 0};
+    static int[] dy = { 0, 0, -1, 1};
+    
+    static int n;
     static int[][] grid, dp;
-    static int answer = 0;
+    
+    static List<Cell> cells = new ArrayList<>();
+    static int ans = 0;
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        
+        grid = new int[n][n];
+        dp = new int[n][n];
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        N = Integer.parseInt(br.readLine());
-
-        grid = new int[N][N];
-        dp = new int[N][N];
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[2], o2[2]));
-
-        for(int i = 0 ; i < N ; i++) {
-            Arrays.fill(dp[i], 1);
-
-            st = new StringTokenizer(br.readLine(), " ");            
-            for(int j = 0 ; j < N ; j++) {
-                grid[i][j] = Integer.parseInt(st.nextToken());
-                pq.offer(new int[] {i, j, grid[i][j]});
+        for(int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], 1);  // 초기 DP 값 모두 1로 초기화
+        
+            for(int j = 0; j < n; j++) {
+                grid[i][j] = sc.nextInt();
+                cells.add(new Cell(grid[i][j], i, j));  // (값, 행, 열)
             }
-        }        
+        }
+        Collections.sort(cells);    //  값 기준 오름차순 정렬⭐ 
 
-        while(!pq.isEmpty()) {
-            int[] now = pq.poll();
-            int x = now[0];
-            int y = now[1];
-            int num = now[2];
+        // 정수값이 작은 칸부터 보며 4방향에 대해 dp 값 갱신
+        for(int i = 0; i < cells.size(); i++) {
+            int x = cells.get(i).x;
+            int y = cells.get(i).y;
 
-            for(int d = 0 ; d < 4 ; d++) {
+            // 인접한 4개의 칸에 대해 갱신
+            for(int d = 0; d < 4; d++) {
                 int nx = x + dx[d];
                 int ny = y + dy[d];
-
-                if(!inRange(nx, ny))    continue;
-
-                if(num < grid[nx][ny]) {
+                
+                if(inRange(nx, ny) && grid[nx][ny] > grid[x][y])
                     dp[nx][ny] = Math.max(dp[nx][ny], dp[x][y] + 1);
-                }
             }
-
-            answer = Math.max(answer, dp[x][y]);
         }
 
-        System.out.println(answer);
+        // 전체 수들 중 최댓값 탐색
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                ans = Math.max(ans, dp[i][j]);
+
+        System.out.print(ans);
+    }
+    
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
+}
+
+
+class Cell implements Comparable<Cell> {
+    int num, x, y;
+
+    public Cell(int num, int x, int y) {
+        this.num = num;
+        this.x = x;
+        this.y = y;
     }
 
-    private static boolean inRange(int x, int y) {
-        return (0 <= x && x < N && 0 <= y && y < N);
+    @Override
+    public int compareTo(Cell c) {
+        return this.num - c.num;      // num 기준 오름차순 정렬
     }
 }
