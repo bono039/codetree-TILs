563e04dd45e12e7a302f5f45f6fe67a77f8fc123
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-01-27 22:01:59 +0900

    Title: 아름다운 수, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

--- 240127/아름다운 수/beautiful-number.java
+++ 240127/아름다운 수/beautiful-number.java
@@ -3,14 +3,14 @@ import java.io.*;
 
 public class Main {
     static int N;
-    static List<Integer> list = new ArrayList<>();         // 뽑은 숫자 넣는 리스트
-    static Map<Integer, Integer> map = new HashMap<>();    // 해당 숫자에 어떤 숫자가 몇 번 들어있는지 확인하는 용
+    static int[] arr;
     static int cnt = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
+        arr = new int[N];
 
         dfs(0);
         System.out.println(cnt);
@@ -18,21 +18,20 @@ public class Main {
 
     private static void dfs(int depth) {
         if(depth == N) {
-            for(int i : list) {
-                map.put(i, map.getOrDefault(i, 0) + 1);
-            }
-
-            for(int v : map.keySet()) {
-                if(map.get(v) == v) {
-                    cnt++;
-                }
-            }
+            cnt++;
+            return;
         }
 
-        for(int i = 1 ; i <= 9 ; i++) {
-            list.add(i);
-            dfs(depth + 1);
-            list.remove(list.size() - 1);
+        for(int i = 1 ; i <= 4 ; i++) {
+            if(depth + i > N) {
+                return;
+            }
+            else {
+                for(int j = 0 ; j < i ; j++) {
+                    arr[depth + j] = i;
+                }
+                dfs(depth + i);
+            }
         }
     }
 }
