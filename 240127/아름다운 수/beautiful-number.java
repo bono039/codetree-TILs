3f4cb146844f68862cbd231e014d7b99b5c76fe5
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-01-27 22:10:29 +0900

    Title: 아름다운 수, Time: 197ms, Memory: 11MB, Status: Passed - CodeTree

--- 240127/아름다운 수/beautiful-number.java
+++ 240127/아름다운 수/beautiful-number.java
@@ -3,14 +3,12 @@ import java.io.*;
 
 public class Main {
     static int N;
-    static int[] arr;
+    static ArrayList<Integer> list = new ArrayList<>();
     static int cnt = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
-        arr = new int[N];
 
         dfs(0);
         System.out.println(cnt);
@@ -18,20 +16,29 @@ public class Main {
 
     private static void dfs(int depth) {
         if(depth == N) {
-            cnt++;
+            if(isBeautiful())
+                cnt++;
             return;
         }
 
         for(int i = 1 ; i <= 4 ; i++) {
-            if(depth + i > N) {
-                return;
+            list.add(i);
+            dfs(depth + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    private static boolean isBeautiful() {
+        for(int i = 0 ; i < N ; i += list.get(i)) {
+            if(i + list.get(i) > N) {
+                return false;
             }
-            else {
-                for(int j = 0 ; j < i ; j++) {
-                    arr[depth + j] = i;
-                }
-                dfs(depth + i);
+            for(int j = i ; j < i + list.get(i) ; j++) {
+                if(list.get(j) != list.get(i))
+                    return false;
             }
         }
+
+        return true;
     }
 }
