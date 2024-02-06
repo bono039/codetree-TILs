c2826a1d0bf127e09af8aa209043b5b8503002ab
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-02-06 16:55:05 +0900

    Title: 금 채굴하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240206/금 채굴하기/gold-mining.java
+++ 240206/금 채굴하기/gold-mining.java
@@ -2,9 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
-
     static int n, m;
     static int[][] map;
     static int max = 0; // 손해보지 않으면서 채굴 가능한 최대 금 개수
@@ -24,38 +21,28 @@ public class Main {
             }
         }
 
-        // K가 0이면 한 점에서만 채굴
-        // 채굴에 드는 비용 : K * K + (K + 1) * (K + 1)
-        for(int k = 0 ; k < n ; k++) {
-            // 마름모 중심점 위치
-            for(int i = 0 ; i < n ; i++) {
-                for(int j = 0 ; j < n ; j++) {
-                    int goldCnt = countGold(i, j, k);
-                    int tmpPrice = (m * goldCnt) - (k * k + (k + 1) * (k + 1));
+        for(int i = 0 ; i < n ; i++) {
+            for(int j = 0 ; j < n ; j++) {
+                for(int k = 0 ; k < n ; k++) {
+                    int goldCnt = countGold(i,j,k);
 
-                    if(tmpPrice >= 0)
+                    if(goldCnt * m >= digCost(k)) {
                         max = Math.max(max, goldCnt);
-
-                }
+                    }
+                }                    
             }
         }
 
         System.out.println(max);
     }
 
-    private static int countGold(int x, int y, int len) {
-        boolean[][] visited = new boolean[n][n];
-
+    private static int countGold(int x, int y, int k) {
         int cnt = 0;
 
-        for(int k = 0 ; k <= len ; k++) {
-            for(int d = 0 ; d < 4 ; d++) {
-                int nx = x + dx[d] * k;
-                int ny = y + dy[d] * k;
-
-                if(inRange(nx, ny) && !visited[nx][ny] && map[nx][ny] == 1) {
-                    cnt++;
-                    visited[nx][ny] = true;
+        for(int i = 0 ; i < n ; i++) {
+            for(int j = 0 ; j < n ; j++) {
+                if(Math.abs(x - i) + Math.abs(y - j) <= k) {
+                    cnt += map[i][j];
                 }
             }
         }
@@ -63,7 +50,7 @@ public class Main {
         return cnt;
     }
 
-    private static boolean inRange(int x, int y) {
-        return (0 <= x && x < n && 0 <= y && y < n);
+    private static int digCost(int k) {
+        return k * k + (k + 1) * (k + 1);
     }
 }
